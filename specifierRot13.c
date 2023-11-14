05c5926b1b7066a03b60911f72e79b63b58d52a5
Jocseh <josephmkaranja07@gmail.com>
2023-11-14 17:58:39 +0300

    task 14 rot13

--- specifierRot13.c
+++ specifierRot13.c
@@ -10,22 +10,21 @@ int specifierRot13(va_list stringList)
 {
 	int i, j, k = 0, total = 0;
 	char *s = va_arg(stringList, char *);
-	char *a = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char *b = " nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
 	if (s == NULL)
 		s = "(null)";
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i]; i++)
 	{
 		k = 0;
-		for (j = 0; a[j] != '\n' && k == 0; j++)
+		for (j = 0; a[j] && k == 0; j++)
 		{
 			if (s[i] == a[j])
 			{
 				_putchar(b[j]);
 				total++;
 				k = 1;
-				break;
 			}
 		}
 		if (k == 0)
@@ -33,7 +32,7 @@ int specifierRot13(va_list stringList)
 			_putchar(s[i]);
 			total++;
 		}
-
 	}
+
 	return (total);
 }
