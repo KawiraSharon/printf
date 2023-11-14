97490b37bcb57893916743edf168cccad37446c9
Jocseh <josephmkaranja07@gmail.com>
2023-11-14 17:34:17 +0300

    task 13

--- specifierRot13.c
+++ specifierRot13.c
@@ -8,7 +8,7 @@
 
 int specifierRot13(va_list stringList)
 {
-	int i, j, total;
+	int i, j, k, total;
 	char *s = va_arg(stringList, char *);
 	char a[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
 	char b[] = {"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"};
@@ -17,20 +17,23 @@ int specifierRot13(va_list stringList)
 		s = "(NULL)";
 	for (i = 0; s[i]; i++)
 	{
+		k = 0;
 		for (j = 0; a[j]; j++)
 		{
 			if (s[i] == a[j])
 			{
 				_putchar(b[j]);
 				total++;
+				k = 1;
 				break;
 			}
-			else
-			{
-				_putchar(s[i]);
-				total++;
-			}
 		}
+		if (k == 0)
+		{
+			_putchar(s[i]);
+			total++;
+		}
+
 	}
 	return (total);
 }
